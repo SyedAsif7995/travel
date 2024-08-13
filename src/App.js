57704d47799407299098df80676c0a028facad36
Syed Asif <asifsyed799549@gmail.com>
2024-08-13 12:38:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import Loader from 'react-loader-spinner'
+import './App.css'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {isLoading: true, dataList: []}
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData = async () => {
+    const url = 'https://apis.ccbp.in/tg/packages'
+    const options = {method: 'GET'}
+    const response = await fetch(url, options)
+    if (response.ok === true) {
+      const data = await response.json()
+      const formatData = data.packages.map(each => ({
+        id: each.id,
+        name: each.name,
+        imageUrl: each.image_url,
+        description: each.description,
+      }))
+      this.setState({isLoading: false, dataList: formatData})
+      console.log(formatData)
+    }
+  }
+
+  loadingView = () => (
+    <div data-testid="loader" className="loader-con">
+      <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
     </div>
-  );
+  )
+
+  successView = () => {
+    const {dataList} = this.state
+    return (
+      <ul className="list-con">
+        {dataList.map(e => (
+          <Home details={e} key={e.id} />
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+    return (
+      <div className="main-con">
+        <h1 className="heading">Travel Guide</h1>
+        <div className="jp">
+          {isLoading === true ? this.loadingView() : this.successView()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
